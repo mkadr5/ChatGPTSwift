b50ec79d58ba926b9c63507f06031c82c4806e92
mkadr5 <muhammetkadir52@gmail.com>
2023-05-24 16:57:25 +0300

    Initial Commit

---
+++ ChatGPTSwift/ChatGPTSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTSwiftApp.swift
+//  ChatGPTSwift
+//
+//  Created by Muhammet Kadir on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
